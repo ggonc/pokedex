ce85d4e67828ea1f83476f277ffe82cb598760d0
ggonc <guilherme.gchagas@outlook.com>
2023-03-27 11:47:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pokedex
+Esse projeto tem como objetivo ...
+
+### Acesso
+Acessado através do http-server
+- Instalação:   npm install -g http-server
+- Execução:     http-server [./]
---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">Grass</li>
+                    <li class="type">Poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png" 
+                alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
